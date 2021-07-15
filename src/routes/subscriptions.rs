0751b99f05d39e6b6b11fa7c310e1c192b81d4bc
yassun <yassun910@gmail.com>
2021-07-15 10:23:29 +0900

    add tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,10 +9,29 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        email = %form.email,
+        name = %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
+    insert_subscriber(&pool, &form)
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,11 +42,11 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(pool.as_ref())
+    .execute(pool)
     .await
     .map_err(|e| {
-        println!("Failed to execute query: {}", e);
-        HttpResponse::InternalServerError().finish()
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
     })?;
-    Ok(HttpResponse::Ok().finish())
+    Ok(())
 }
